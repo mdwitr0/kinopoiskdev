36b6aaab3db4b6e54a07bd4643f31a8bf70b321d
mdwitr0 <mdwit0r@gmail.com>
2023-01-31 20:44:15 +0100

    init services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MovieModule } from './movie/movie.module';
+import { PersonModule } from './person/person.module';
+import { ImageModule } from './image/image.module';
+import { ReviewModule } from './review/review.module';
+import { SeasonModule } from './season/season.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MovieModule, PersonModule, ImageModule, ReviewModule, SeasonModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
