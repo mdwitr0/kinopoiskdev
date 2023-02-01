2c4e393567dca55857cb02445e7061c64a7a12d3
mdwitr0 <mdwit0r@gmail.com>
2023-02-01 19:14:36 +0100

    add review schema

---
+++ src/review/schemas/review.schema.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+@Schema()
+export class Review extends Document {
+  @Prop({ required: true, unique: true, index: true })
+  id: number;
+  @Prop({ required: true, index: true })
+  movieId: number;
+
+  @Prop()
+  title: string;
+
+  @Prop()
+  type: string;
+
+  @Prop()
+  review: string;
+
+  @Prop()
+  date: Date;
+
+  @Prop()
+  author: string;
+
+  @Prop({ default: 0 })
+  userRating: number;
+
+  @Prop()
+  authorId: number;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(Review);
+ReviewSchema.plugin(mongoosePaginate);
