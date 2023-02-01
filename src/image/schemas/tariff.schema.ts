6076cb830bd880a20403edc759acb364fa3e0134
mdwitr0 <mdwit0r@gmail.com>
2023-02-01 19:04:55 +0100

    add image schema

---
+++ src/image/schemas/tariff.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+@Schema()
+export class Image {
+  @Prop({ required: true, index: true })
+  movieId: number;
+
+  @Prop()
+  type?: string;
+
+  @Prop()
+  language?: string;
+
+  @Prop({ unique: true })
+  url: string;
+
+  @Prop()
+  previewUrl?: string;
+
+  @Prop()
+  height?: number;
+
+  @Prop()
+  width?: number;
+}
+export const ImageSchema = SchemaFactory.createForClass(Image);
+ImageSchema.plugin(mongoosePaginate);
