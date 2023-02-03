c083e43f9a8d17d500e7d3708436b44edde43225
mdwitr0 <mdwit0r@gmail.com>
2023-02-03 22:00:06 +0100

    add season schema

---
+++ src/season/schemas/season.schema.ts
@@ -0,0 +1,43 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+export class Episode {
+  @Prop()
+  movieId: number;
+
+  @Prop()
+  seasonNumber?: number;
+
+  @Prop()
+  episodeNumber?: number;
+
+  @Prop()
+  name?: string;
+
+  @Prop()
+  alternativeName?: string;
+
+  @Prop()
+  description?: string;
+
+  @Prop()
+  date?: Date;
+}
+
+@Schema({
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+})
+export class SeasonModel extends Document {
+  @Prop({ index: true })
+  movieId: number;
+  @Prop()
+  number: number;
+  @Prop()
+  episodesCount: number;
+  @Prop({ type: () => [Episode] })
+  episodes: Episode[];
+}
+
+const SeasonSchema = SchemaFactory.createForClass(Episode);
+SeasonSchema.plugin(mongoosePaginate);
