0cbf3a43edaa49f9ae76aa3bb315d029727bb7c1
mdwitr0 <mdwit0r@gmail.com>
2023-02-04 14:25:12 +0100

    fix: api not generated literal types

--- src/common/decorators/api-dot-notation-query.decorator.ts
+++ src/common/decorators/api-dot-notation-query.decorator.ts
@@ -1,34 +1,38 @@
 /* eslint-disable @typescript-eslint/ban-types*/
 
-import { applyDecorators } from '@nestjs/common';
+import { applyDecorators, SetMetadata } from '@nestjs/common';
 import { ApiQuery, ApiQueryOptions } from '@nestjs/swagger';
 import { SortTypeEnum } from '../enum/sort.enum';
 
 const getPagingDecorators = (fn: Function) => {
   const constructor = fn.prototype;
-  const properties = Reflect.getMetadata(
-    'swagger/apiModelPropertiesArray',
-    constructor,
-  ).map((prop) => prop.substr(1));
-
-  return properties.flatMap((property) => {
-    const meta = Reflect.getMetadata(
-      'swagger/apiModelProperties',
+  if (Reflect.getMetadata('swagger/apiModelPropertiesArray', constructor)) {
+    const properties = Reflect.getMetadata(
+      'swagger/apiModelPropertiesArray',
       constructor,
-      property,
-    );
-    const subClass = meta.type();
-    if (subClass) {
-      return getPagingDecorators(subClass);
-    }
-    return [
-      ApiQuery({
-        name: property,
-        type: typeof subClass,
-        ...meta,
-      }),
-    ];
-  });
+    ).map((prop) => prop.substr(1));
+
+    return properties.flatMap((property) => {
+      const meta = Reflect.getMetadata(
+        'swagger/apiModelProperties',
+        constructor,
+        property,
+      );
+      const subClass = meta.type();
+      if (subClass) {
+        return getPagingDecorators(subClass);
+      }
+      return [
+        ApiQuery({
+          name: property,
+          type: typeof subClass,
+          ...meta,
+        }),
+      ];
+    });
+  } else {
+    return [];
+  }
 };
 
 const getQueryDecorators = (
@@ -37,6 +41,7 @@ const getQueryDecorators = (
   valueQuery: ApiQueryOptions,
 ) => {
   const constructor = fn.prototype;
+
   const properties = Reflect.getMetadata(
     'swagger/apiModelPropertiesArray',
     constructor,
@@ -49,20 +54,22 @@ const getQueryDecorators = (
       property,
     );
 
-    if (typeof meta.type !== 'function') return [property];
-    const subClass = meta.type();
+    const type = meta.type();
 
-    if (typeof subClass === 'function') {
-      const subClassProperties = Reflect.getMetadata(
-        'swagger/apiModelPropertiesArray',
-        subClass.prototype,
-      ).map((prop) => prop.substr(1));
+    switch (type.name) {
+      case 'String':
+      case 'Number':
+      case 'Boolean':
+        return [property];
+      default:
+        const subClassProperties = Reflect.getMetadata(
+          'swagger/apiModelPropertiesArray',
+          type.prototype,
+        )?.map((prop) => prop.substr(1));
 
-      return subClassProperties.map((subClassProperty) => {
-        return `${property}.${subClassProperty}`;
-      });
-    } else {
-      return [property];
+        return subClassProperties?.map((subClassProperty) => {
+          return `${property}.${subClassProperty}`;
+        });
     }
   });
 
