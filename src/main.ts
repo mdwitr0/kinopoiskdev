df20520f058a1629026062cfc07b07fe600c8339
mdwitr0 <mdwit0r@gmail.com>
2023-02-04 01:05:08 +0100

    add fastify, swagger, helmet & compressor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,71 @@
-import { NestFactory } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyHelmet from '@fastify/helmet';
+import compression from '@fastify/compress';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const docGlobalPrefix = 'documentation';
+
+  // Init logger
+  const logger = new Logger('Main');
+  // Init fastify adapter
+  const adapter = new FastifyAdapter({
+    trustProxy: true,
+  });
+
+  // Set global helmet
+  adapter.register(fastifyHelmet, { global: true });
+  // Set global gzip compression
+  adapter.register(compression, {
+    global: true,
+    encodings: ['gzip', 'deflate'],
+  });
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+    { bufferLogs: true },
+  );
+
+  // Set global version
+  app.enableVersioning({ defaultVersion: '1', type: VersioningType.URI });
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription(
+      'Документация для API kinopoisk.dev. API позволяет получать максимум информации с кинопоиска о фильмах, сериалах и персонах и т.д.',
+    )
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(docGlobalPrefix, app, document);
+
+  const port = process.env.PORT || 3000;
+  const appEnv = process.env.APP_ENV;
+  await app.listen(port);
+  logger.log(`🚀 API app is running on: http://localhost:${port}/`);
+  logger.log(`🌚 ENV: ${appEnv}`);
+  logger.log(
+    `📑 API Documentation is running on: http://localhost:${port}/${docGlobalPrefix}`,
+  );
 }
+
 bootstrap();
