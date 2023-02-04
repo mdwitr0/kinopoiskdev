65a3d29756d21bd1393d8fd5e45eb51cc414924e
mdwitr0 <mdwit0r@gmail.com>
2023-02-04 16:43:54 +0100

    add api docs to reviews

--- src/review/schemas/review.schema.ts
+++ src/review/schemas/review.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 @Schema({
   timestamps: true,
@@ -8,32 +9,40 @@ import mongoosePaginate from 'mongoose-paginate-v2';
   toObject: { virtuals: true },
 })
 export class Review extends Document {
+  @ApiPropertyOptional()
   @Prop({ required: true, unique: true, index: true })
   id: number;
+
+  @ApiPropertyOptional()
   @Prop({ required: true, index: true })
   movieId: number;
 
+  @ApiPropertyOptional()
   @Prop()
   title: string;
 
+  @ApiPropertyOptional()
   @Prop()
   type: string;
 
+  @ApiPropertyOptional()
   @Prop()
   review: string;
 
+  @ApiPropertyOptional()
   @Prop()
   date: Date;
 
+  @ApiPropertyOptional()
   @Prop()
   author: string;
 
   @Prop({ default: 0 })
   userRating: number;
 
+  @ApiPropertyOptional()
   @Prop()
   authorId: number;
 }
 
 export const ReviewSchema = SchemaFactory.createForClass(Review);
-ReviewSchema.plugin(mongoosePaginate);
