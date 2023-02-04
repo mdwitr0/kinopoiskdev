4a4371d5c4f992237b1942d9a8e5be5d9890a46e
mdwitr0 <mdwit0r@gmail.com>
2023-02-04 14:35:41 +0100

    set api decorators to season model

--- src/season/schemas/season.schema.ts
+++ src/season/schemas/season.schema.ts
@@ -1,44 +1,60 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoosePaginate from 'mongoose-paginate-v2';
+import { HydratedDocument } from 'mongoose';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class Episode {
+  @ApiPropertyOptional()
   @Prop()
   movieId: number;
 
+  @ApiPropertyOptional()
   @Prop()
   seasonNumber?: number;
 
+  @ApiPropertyOptional()
   @Prop()
   episodeNumber?: number;
 
+  @ApiPropertyOptional()
   @Prop()
   name?: string;
 
+  @ApiPropertyOptional()
   @Prop()
   alternativeName?: string;
 
+  @ApiPropertyOptional()
   @Prop()
   description?: string;
 
+  @ApiPropertyOptional()
   @Prop()
   date?: Date;
 }
+
+export type SeasonDocument = HydratedDocument<Season>;
 
 @Schema({
   timestamps: true,
   toJSON: { virtuals: true },
   toObject: { virtuals: true },
 })
-export class SeasonModel extends Document {
+export class Season {
+  @ApiPropertyOptional()
   @Prop({ index: true })
   movieId: number;
+
+  @ApiPropertyOptional()
   @Prop()
   number: number;
+
+  @ApiPropertyOptional()
   @Prop()
   episodesCount: number;
+
+  @ApiPropertyOptional({ type: () => Episode, isArray: true })
   @Prop({ type: () => [Episode] })
   episodes: Episode[];
 }
 
 const SeasonSchema = SchemaFactory.createForClass(Episode);
-SeasonSchema.plugin(mongoosePaginate);
