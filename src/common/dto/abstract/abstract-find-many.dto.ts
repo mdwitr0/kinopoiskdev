24d070b43a2fd3c53fbf5aef132cdb05d98f5100
mdwitr0 <mdwit0r@gmail.com>
2023-02-04 10:53:39 +0100

    add abstract find many

---
+++ src/common/dto/abstract/abstract-find-many.dto.ts
@@ -0,0 +1,33 @@
+import { IsOptional, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+type Constructor<T> = new (...args: any[]) => T;
+
+export function AbstractFindManyDto<TQueryDto, TSortDto, TPaginationDto>(
+  QueryDto: Constructor<TQueryDto>,
+  SortDto: Constructor<TSortDto>,
+  PaginationDto: Constructor<TPaginationDto>
+) {
+  abstract class AbstractFindManyDto {
+    @Type(() => QueryDto)
+    @IsOptional()
+    @ValidateNested()
+    public query: TQueryDto;
+
+    @Type(() => SortDto)
+    @IsOptional()
+    @ValidateNested()
+    public sortQuery: TSortDto;
+
+    @Type(() => PaginationDto)
+    @IsOptional()
+    @ValidateNested()
+    public pagination: TPaginationDto;
+
+    protected constructor(partials: Partial<AbstractFindManyDto>) {
+      Object.assign(this, partials);
+    }
+  }
+
+  return AbstractFindManyDto;
+}
