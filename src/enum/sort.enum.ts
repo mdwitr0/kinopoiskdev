9e05e7ec229b6832b44a01f1d780dd25f1961bf3
mdwitr0 <mdwit0r@gmail.com>
2023-02-04 10:37:06 +0100

    add sort enum

---
+++ src/enum/sort.enum.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+export enum SortTypeEnum {
+  ASC = 'ASC',
+  DESC = 'DESC',
+  asc = 'asc',
+  desc = 'desc',
+  '1' = '1',
+  '-1' = '-1',
+}
