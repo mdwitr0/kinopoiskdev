4cfda411b86f67f88f5b32eac3665d75fe8252fe
mdwitr0 <mdwit0r@gmail.com>
2023-02-05 16:58:07 +0100

    add api docs to person

--- src/person/schemas/person.schema.ts
+++ src/person/schemas/person.schema.ts
@@ -1,69 +1,88 @@
 import mongoosePaginate from 'mongoose-paginate-v2';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 class BirthPlace {
+  @ApiPropertyOptional()
   @Prop()
   value: string;
 }
 
 class DeathPlace {
+  @ApiPropertyOptional()
   @Prop()
   value: string;
 }
 
 class Spouses {
+  @ApiPropertyOptional()
   @Prop()
   id: number;
 
+  @ApiPropertyOptional()
   @Prop()
   name: string;
 
+  @ApiPropertyOptional()
   @Prop()
   divorced: boolean;
 
+  @ApiPropertyOptional()
   @Prop()
   divorcedReason: string;
 
+  @ApiPropertyOptional()
   @Prop()
   sex: string;
 
+  @ApiPropertyOptional()
   @Prop()
   children: number;
 
+  @ApiPropertyOptional()
   @Prop()
   relation: string;
 }
 
 class Profession {
+  @ApiPropertyOptional()
   @Prop()
   value: string;
 }
 
 class Fact {
+  @ApiPropertyOptional()
   @Prop()
   value: string;
 }
 
 class Movie {
+  @ApiPropertyOptional()
   @Prop({ index: true })
   id: number;
 
+  @ApiPropertyOptional()
   @Prop()
   name: string;
 
+  @ApiPropertyOptional()
   @Prop()
   alternativeName: string;
 
+  @ApiPropertyOptional()
   @Prop()
   rating: number;
 
+  @ApiPropertyOptional()
   @Prop()
   General: boolean;
 
+  @ApiPropertyOptional()
   @Prop()
   description: string;
 
+  @ApiPropertyOptional()
   @Prop()
   enProfession: string;
 }
@@ -73,58 +92,74 @@ class Movie {
   toJSON: { virtuals: true },
   toObject: { virtuals: true },
 })
-class Person extends Document {
+export class Person {
+  @ApiPropertyOptional()
   @Prop({ required: true, unique: true, index: true })
   id: number;
 
+  @ApiPropertyOptional()
   @Prop()
   name: string;
 
+  @ApiPropertyOptional()
   @Prop()
   enName: string;
 
+  @ApiPropertyOptional()
   @Prop()
   photo: string;
 
+  @ApiPropertyOptional()
   @Prop({ index: true })
   sex: string;
 
+  @ApiPropertyOptional()
   @Prop()
   growth: number;
 
+  @ApiPropertyOptional()
   @Prop({ index: true })
   birthday: Date;
 
+  @ApiPropertyOptional()
   @Prop({ index: true })
   death: Date;
 
+  @ApiPropertyOptional()
   @Prop({ index: true })
   age: number;
 
+  @ApiPropertyOptional({ type: () => BirthPlace, isArray: true })
   @Prop({ items: BirthPlace })
   birthPlace: BirthPlace[];
 
+  @ApiPropertyOptional({ type: () => DeathPlace, isArray: true })
   @Prop({ items: DeathPlace })
   deathPlace: DeathPlace[];
 
+  @ApiPropertyOptional()
   @Prop()
   spouses: Spouses;
 
+  @ApiPropertyOptional()
   @Prop()
   countAwards: number;
 
+  @ApiPropertyOptional({ type: () => Profession, isArray: true })
   @Prop({ items: Profession })
   profession: Profession[];
 
+  @ApiPropertyOptional({ type: () => Fact, isArray: true })
   @Prop({ items: Fact })
   facts: Fact[];
 
+  @ApiPropertyOptional({ type: () => Movie, isArray: true })
   @Prop({ items: Movie })
   movies: Movie[];
 
+  @ApiPropertyOptional()
   @Prop({ default: false })
   isParse: boolean;
 }
 
 export const PersonSchema = SchemaFactory.createForClass(Person);
-PersonSchema.plugin(mongoosePaginate);
