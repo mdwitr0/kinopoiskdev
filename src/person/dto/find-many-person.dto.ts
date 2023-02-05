336bc7ea1184d2bc1c6613bc8de0f0aa121e0058
mdwitr0 <mdwit0r@gmail.com>
2023-02-05 16:58:41 +0100

    add person dto

---
+++ src/person/dto/find-many-person.dto.ts
@@ -0,0 +1,14 @@
+import { AbstractFindManyDto } from '../../common/dto/abstract/abstract-find-many.dto';
+import { PaginatedQueryDto } from '../../common/dto/query/paginated.query.dto';
+import { Person } from '../schemas/person.schema';
+
+export class FindManyPersonDto extends AbstractFindManyDto(
+  Person,
+  Person,
+  PaginatedQueryDto,
+) {
+  constructor(partial: Partial<FindManyPersonDto>) {
+    super(partial);
+    Object.assign(this, partial);
+  }
+}
---
+++ src/person/dto/person-docs.response.dto.ts
@@ -0,0 +1,10 @@
+import { PartialType } from '@nestjs/swagger';
+import { AbstractDocsResponseDto } from '../../common/dto/abstract/abstract-docs.response.dto';
+import { Person } from '../schemas/person.schema';
+
+export class PersonDocsResponseDto extends AbstractDocsResponseDto(Person) {
+  constructor(partial: Partial<PersonDocsResponseDto>) {
+    super(partial);
+    Object.assign(this, partial);
+  }
+}
