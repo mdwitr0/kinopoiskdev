69ee6e7fb642836930ec4b701ba5cdceae579ec5
mdwitr0 <mdwit0r@gmail.com>
2023-02-05 16:56:56 +0100

    add api docs to image

--- src/image/schemas/image.schema.ts
+++ src/image/schemas/image.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoosePaginate from 'mongoose-paginate-v2';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 @Schema({
   timestamps: true,
@@ -7,26 +7,32 @@ import mongoosePaginate from 'mongoose-paginate-v2';
   toObject: { virtuals: true },
 })
 export class Image {
+  @ApiProperty()
   @Prop({ required: true, index: true })
   movieId: number;
 
+  @ApiPropertyOptional()
   @Prop()
   type?: string;
 
+  @ApiPropertyOptional()
   @Prop()
   language?: string;
 
+  @ApiPropertyOptional()
   @Prop({ unique: true })
   url: string;
 
+  @ApiPropertyOptional()
   @Prop()
   previewUrl?: string;
 
+  @ApiPropertyOptional()
   @Prop()
   height?: number;
 
+  @ApiPropertyOptional()
   @Prop()
   width?: number;
 }
 export const ImageSchema = SchemaFactory.createForClass(Image);
-ImageSchema.plugin(mongoosePaginate);
