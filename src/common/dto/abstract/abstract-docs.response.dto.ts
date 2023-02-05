2f814c67dd8a16cecd42ed4209cd7ec8ae47b0d1
mdwitr0 <mdwit0r@gmail.com>
2023-02-05 16:59:54 +0100

    remove unused decorators

--- src/common/dto/abstract/abstract-docs.response.dto.ts
+++ src/common/dto/abstract/abstract-docs.response.dto.ts
@@ -4,27 +4,22 @@ import { ApiProperty } from '@nestjs/swagger';
 type Constructor<T> = new (...args: any[]) => T;
 
 export function AbstractDocsResponseDto<TEntityDto, T>(
-  EntityDto: Constructor<TEntityDto>
+  EntityDto: Constructor<TEntityDto>,
 ) {
   abstract class AbstractDocsResponseDto {
-    @Expose()
     @ApiProperty({ type: [EntityDto] })
     @Type(() => EntityDto)
     public docs: TEntityDto[];
 
-    @Expose()
     @ApiProperty({ description: 'Общее количество результатов' })
     public total: number;
 
-    @Expose()
     @ApiProperty({ description: 'Количество результатов на странице' })
     public limit: number;
 
-    @Expose()
     @ApiProperty({ description: 'Текущая страница' })
     public page: number;
 
-    @Expose()
     @ApiProperty({ description: 'Сколько страниц всего' })
     public pages: number;
 
