92b2bc1523f03e8d25d85485a85d632444fd6d2f
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-17 00:56:49 +0100

    feat: add base controller

---
+++ src/common/base/base.controller.ts
@@ -0,0 +1,30 @@
+import { ApiResponse } from '@nestjs/swagger';
+import { Get, Param, Query } from '@nestjs/common';
+import { IQuery } from '../interfaces/query.interface';
+import { PaginatedDecorator } from '../decorators/find-many.decorator';
+
+type Constructor<T> = new (...args: any[]) => T;
+
+export function BaseController<TEntity, TEntityDto>(
+  Entity: Constructor<TEntity>,
+  EntityDto: Constructor<TEntityDto>,
+  description?: string,
+) {
+  abstract class BaseController {
+    protected constructor(readonly service: any) {}
+
+    @Get()
+    @PaginatedDecorator(Entity, EntityDto, description)
+    async finManyByQuery(@Query() query: IQuery): Promise<TEntityDto> {
+      return this.service.findMany(query);
+    }
+
+    @ApiResponse({ type: Entity })
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<TEntity> {
+      return this.service.findOne(+id);
+    }
+  }
+
+  return BaseController;
+}
