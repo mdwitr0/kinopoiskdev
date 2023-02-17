b80fcf2a3233e92e664fabecf541583af4050867
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-17 01:14:12 +0100

    refactor(image): remove duplicate methods

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,41 +1,16 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Query,
-  SerializeOptions,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
-import { ApiDotNotationQuery } from '../common/decorators/api-dot-notation-query.decorator';
-import { FindManyImageDto } from './dto/find-many-image.dto';
 import { ImageService } from './image.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ParseDotNotationQuery } from '../common/pipes/parse-dot-notation-query.pipe';
-import { PaginatedQueryDto } from '../common/dto/query/paginated.query.dto';
 import { ImageDocsResponseDto } from './dto/image-docs.response.dto';
 import { Image } from './schemas/image.schema';
-import { ToolsQueryDto } from '../common/dto/query/tools.query.dto';
-import { IQuery } from 'src/common/interfaces/query.interface';
-import { QueryPipe } from 'src/common/pipes/query.pipe';
-@SerializeOptions({ excludeExtraneousValues: true })
-@ApiTags('Image')
-@Controller('image')
-export class ImageController {
-  constructor(private readonly imageService: ImageService) {}
+import { BaseController } from 'src/common/base/base.controller';
+import { Controller } from 'src/common/decorators/controller.decorator';
 
-  @Get()
-  @UsePipes(new QueryPipe())
-  @ApiOperation({ summary: 'Поиск изображений' })
-  @ApiDotNotationQuery(ToolsQueryDto, PaginatedQueryDto, Image)
-  @ApiResponse({ type: ImageDocsResponseDto, isArray: true })
-  async finManyByQuery(@Query() query: IQuery): Promise<any> {
-    return this.imageService.findMany(query);
-  }
-
-  @ApiResponse({ type: ImageDocsResponseDto, isArray: true })
-  @Get(':movieId')
-  findOne(@Param('movieId') movieId: string) {
-    return this.imageService.findOne(+movieId);
+@Controller('image', 'Постеры, фоны, кадры, скриншоты и т.д.')
+export class ImageController extends BaseController(
+  Image,
+  ImageDocsResponseDto,
+  'Поиск отзывов',
+) {
+  constructor(private readonly imageService: ImageService) {
+    super(imageService);
   }
 }
