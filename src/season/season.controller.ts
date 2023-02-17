a81221d86c9073733bebc175af7a6a5247fec784
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-17 01:17:20 +0100

    refactor(season): remove duplicate methods

--- src/season/season.controller.ts
+++ src/season/season.controller.ts
@@ -1,39 +1,16 @@
-import {
-  ClassSerializerInterceptor,
-  Controller,
-  Get,
-  Param,
-  Query,
-  SerializeOptions,
-  UseInterceptors,
-} from '@nestjs/common';
-import { SeasonService } from './season.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ApiDotNotationQuery } from '../common/decorators/api-dot-notation-query.decorator';
-import { PaginatedQueryDto } from '../common/dto/query/paginated.query.dto';
+import { BaseController } from 'src/common/base/base.controller';
+import { Controller } from 'src/common/decorators/controller.decorator';
 import { Season } from './schemas/season.schema';
 import { SeasonDocsResponseDto } from './dto/season-docs.response.dto';
-import { ToolsQueryDto } from '../common/dto/query/tools.query.dto';
-import { IQuery } from 'src/common/interfaces/query.interface';
+import { SeasonService } from './season.service';
 
-@UseInterceptors(ClassSerializerInterceptor)
-@SerializeOptions({ excludeExtraneousValues: true })
-@ApiTags('Season')
-@Controller('season')
-export class SeasonController {
-  constructor(private readonly seasonService: SeasonService) {}
-
-  @Get()
-  @ApiOperation({ summary: 'Поиск сезонов' })
-  @ApiDotNotationQuery(ToolsQueryDto, PaginatedQueryDto, Season)
-  @ApiResponse({ type: SeasonDocsResponseDto, isArray: true })
-  async finManyByQuery(@Query() query: IQuery) {
-    return this.seasonService.findMany(query);
-  }
-
-  @ApiResponse({ type: SeasonDocsResponseDto, isArray: true })
-  @Get(':movieId')
-  findOne(@Param('movieId') movieId: string) {
-    return this.seasonService.findOne(+movieId);
+@Controller('season', 'Сезоны и эпизоды')
+export class SeasonController extends BaseController(
+  Season,
+  SeasonDocsResponseDto,
+  'Поиск сезонов',
+) {
+  constructor(private readonly seasonService: SeasonService) {
+    super(seasonService);
   }
 }
