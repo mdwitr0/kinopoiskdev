fed4cb8bc9f0f6190cd8edb0a97b966ad0c6e03b
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-18 00:17:34 +0100

    feat(auth): add auth middleware

---
+++ src/auth/middleware/auth.middleware.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import * as ApiKey from 'uuid-apikey';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly authService: AuthService) {}
+
+  async use(req: FastifyRequest['raw'], res: FastifyReply['raw'], next: () => void) {
+    const token = req?.headers['x-api-key'] || req['query']['token'];
+
+    if (!token) throw new UnauthorizedException('В запросе не указан токен!');
+    // @ts-ignore
+    if (!ApiKey.isAPIKey(token)) throw new UnauthorizedException('Переданный токен некорректен!');
+    const user = await this.authService.findUserByToken(token as string);
+
+    req['user'] = user;
+    next();
+  }
+}
