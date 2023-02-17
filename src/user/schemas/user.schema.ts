d091f7dab73b1367281efd29f23c933d605d17dc
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-17 19:23:17 +0100

    feat(user): add user model

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,63 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument, Types } from 'mongoose';
+import { Tariff } from './tariff.schema';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  userId: number;
+
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  token?: string;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  password?: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+    default() {
+      return this.userId;
+    },
+  })
+  email?: string;
+
+  @Prop({
+    default: () => 0,
+  })
+  requestsUsed?: number;
+
+  @Prop({
+    default: () => '6016bed198ebf72bc112edae',
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Tariff.name,
+  })
+  tariffId?: Types.ObjectId;
+
+  @Prop({
+    default: () => false,
+  })
+  inChat?: boolean;
+
+  @Prop({ isOptional: true })
+  isSubscribed?: boolean;
+
+  @Prop({ isOptional: true, type: Date })
+  subscriptionStartDate?: Date;
+
+  @Prop({ isOptional: true, type: Date })
+  subscriptionEndDate?: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
