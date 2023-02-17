467f57ae29c707358ba5a9ae8f32ee8ccc62993e
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-17 01:08:25 +0100

    refactor(movie): remove duplicate methods

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,43 +1,17 @@
-import {
-  ClassSerializerInterceptor,
-  Controller,
-  Get,
-  Param,
-  Query,
-  SerializeOptions,
-  UseInterceptors,
-  UsePipes,
-} from '@nestjs/common';
 import { MovieService } from './movie.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ApiDotNotationQuery } from '../common/decorators/api-dot-notation-query.decorator';
-import { PaginatedQueryDto } from '../common/dto/query/paginated.query.dto';
 import { MovieDocsResponseDto } from './dto/movie-docs.response.dto';
 import { Movie } from './schemas/movie.schema';
-import { ToolsQueryDto } from '../common/dto/query/tools.query.dto';
-import { IFindManyMovie } from './interfaces/find-many-movie.interface';
-import { QueryPipe } from 'src/common/pipes/query.pipe';
-import { IQuery } from 'src/common/interfaces/query.interface';
 
-@UseInterceptors(ClassSerializerInterceptor)
-@SerializeOptions({ excludeExtraneousValues: true })
-@ApiTags('Movies')
-@Controller('movie')
-export class MovieController {
-  constructor(private readonly movieService: MovieService) {}
+import { BaseController } from 'src/common/base/base.controller';
+import { Controller } from 'src/common/decorators/controller.decorator';
 
-  @Get()
-  @UsePipes(new QueryPipe())
-  @ApiOperation({ summary: 'Поиск фильмов' })
-  @ApiDotNotationQuery(ToolsQueryDto, PaginatedQueryDto, Movie)
-  @ApiResponse({ type: MovieDocsResponseDto, isArray: true })
-  async finManyByQuery(@Query() query: IQuery): Promise<MovieDocsResponseDto> {
-    return this.movieService.findMany(query);
-  }
-
-  @ApiResponse({ type: Movie })
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Movie> {
-    return this.movieService.findOne(+id);
+@Controller('movie', 'Фильмы, сериалы, и т.д.')
+export class MovieController extends BaseController(
+  Movie,
+  MovieDocsResponseDto,
+  'Поиск тайтлов',
+) {
+  constructor(private readonly movieService: MovieService) {
+    super(movieService);
   }
 }
