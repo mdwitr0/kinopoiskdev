c42cb2a9301413338a51f727822a4bfd337786af
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-17 01:12:00 +0100

    refactor(review): remove duplicate methods

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,39 +1,17 @@
-import {
-  ClassSerializerInterceptor,
-  Controller,
-  Get,
-  Param,
-  Query,
-  SerializeOptions,
-  UseInterceptors,
-} from '@nestjs/common';
 import { ReviewService } from './review.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ApiDotNotationQuery } from '../common/decorators/api-dot-notation-query.decorator';
-import { PaginatedQueryDto } from '../common/dto/query/paginated.query.dto';
 import { Review } from './schemas/review.schema';
 import { ReviewDocsResponseDto } from './dto/review-docs-response.dto';
-import { ToolsQueryDto } from '../common/dto/query/tools.query.dto';
-import { IQuery } from 'src/common/interfaces/query.interface';
 
-@UseInterceptors(ClassSerializerInterceptor)
-@SerializeOptions({ excludeExtraneousValues: true })
-@ApiTags('Review')
-@Controller('review')
-export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+import { BaseController } from 'src/common/base/base.controller';
+import { Controller } from 'src/common/decorators/controller.decorator';
 
-  @Get()
-  @ApiOperation({ summary: 'Поиск отзывов' })
-  @ApiDotNotationQuery(ToolsQueryDto, PaginatedQueryDto, Review)
-  @ApiResponse({ type: ReviewDocsResponseDto, isArray: true })
-  async finManyByQuery(@Query() query: IQuery) {
-    return this.reviewService.findMany(query);
-  }
-
-  @ApiResponse({ type: ReviewDocsResponseDto, isArray: true })
-  @Get(':movieId')
-  findOne(@Param('movieId') movieId: string) {
-    return this.reviewService.findOne(+movieId);
+@Controller('review', 'Отзывы пользователей')
+export class ReviewController extends BaseController(
+  Review,
+  ReviewDocsResponseDto,
+  'Поиск отзывов',
+) {
+  constructor(private readonly reviewService: ReviewService) {
+    super(reviewService);
   }
 }
