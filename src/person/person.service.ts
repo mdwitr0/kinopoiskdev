4b5d3b6aa8076c8cf3bcee6ab854375f6585da24
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-18 17:15:08 +0100

    fix(person): not valide path

--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { BaseService } from 'src/common/base/base.service';
-
-import { Person, PersonDocument } from './schemas/Person.schema';
+import { Person, PersonDocument } from 'src/person/schemas/person.schema';
 
 @Injectable()
 export class PersonService extends BaseService<Person> {
