6d9c1d3efe62fdbce76777dde10f67a80e1728d8
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-02-18 01:58:48 +0100

    feat: set optional entity

--- src/common/decorators/paginated.decorator.ts
+++ src/common/decorators/paginated.decorator.ts
@@ -5,10 +5,12 @@ import { ToolsQueryDto } from '../dto/query/tools.query.dto';
 import { QueryPipe } from '../pipes/query.pipe';
 import { ApiDotNotationQuery } from './api-dot-notation-query.decorator';
 
-export const Paginated = (entity: any, entityDto: any) => {
+export const Paginated = (entityDto: any, entity?: any) => {
   return applyDecorators(
     UsePipes(new QueryPipe()),
-    ApiDotNotationQuery(ToolsQueryDto, PaginatedQueryDto, entity),
+    entity
+      ? ApiDotNotationQuery(ToolsQueryDto, PaginatedQueryDto, entity)
+      : ApiDotNotationQuery(ToolsQueryDto, PaginatedQueryDto),
     ApiResponse({ type: entityDto, isArray: true }),
   );
 };
