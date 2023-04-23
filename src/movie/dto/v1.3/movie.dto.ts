99216a13370fc18a73bb1342b400fb24026bb452
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-04-23 18:37:21 +0200

    feat(movie.dto) add series length properties #90

--- src/movie/dto/v1.3/movie.dto.ts
+++ src/movie/dto/v1.3/movie.dto.ts
@@ -7,4 +7,10 @@ export class MovieDtoV1_3 extends MovieDtoV1 {
     description: 'Признак того, что тайтл находится в прокате',
   })
   ticketsOnSale: boolean;
+
+  @ApiNullableProperty({ example: 155, description: 'Продолжительность всех серий' })
+  totalSeriesLength: number;
+
+  @ApiNullableProperty({ example: 20, description: 'Средняя продолжительность серии' })
+  seriesLength: number;
 }
