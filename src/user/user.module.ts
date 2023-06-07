238996969e8046a47df6b8942ffed3f0cef5f154
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-06-07 21:55:52 +0200

    fix(user): user tokens are not reset

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   providers: [UserService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private readonly logger = new Logger(UserService.name);
+  constructor(@InjectModel(User.name) private readonly userRepository: Model<UserDocument>) {}
+
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  resetRequestsUsed() {
+    this.userRepository.updateMany({}, { requestsUsed: 0 });
+    this.logger.log('Finish: Reset requests used');
+  }
+}
