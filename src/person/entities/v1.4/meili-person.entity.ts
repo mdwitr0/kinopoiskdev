2b6b06354a582dc094cd08b42ca2893f48518988
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-10-20 00:07:04 +0200

    chore: add v1.4 search person model

---
+++ src/person/entities/v1.4/meili-person.entity.ts
@@ -0,0 +1,85 @@
+import { Expose } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional, OmitType } from '@nestjs/swagger';
+import { MeiliPersonEntity } from '../meili-person.entity';
+import { Prop } from '@nestjs/mongoose';
+import { BirthPlace, DeathPlace, Person, Profession } from '../../schemas/person.schema';
+
+export class MeiliPersonEntityV1_4 extends OmitType(MeiliPersonEntity, [
+  'birthPlace',
+  'deathPlace',
+  'profession',
+  'fromMongoDocument',
+] as const) {
+  @ApiProperty()
+  @Expose()
+  id: number;
+
+  @ApiProperty()
+  @Expose()
+  name: string;
+
+  @ApiProperty()
+  @Expose()
+  enName: string;
+
+  @ApiProperty()
+  @Expose()
+  photo: string;
+
+  @ApiProperty()
+  @Expose()
+  sex: string;
+
+  @ApiProperty()
+  @Expose()
+  growth: number;
+
+  @ApiProperty()
+  @Expose()
+  birthday: string;
+
+  @ApiProperty()
+  @Expose()
+  death: string;
+
+  @ApiProperty()
+  @Expose()
+  age: number;
+
+  @ApiPropertyOptional({ type: () => BirthPlace, isArray: true })
+  @Prop({ items: BirthPlace })
+  birthPlace: BirthPlace[];
+
+  @ApiPropertyOptional({ type: () => DeathPlace, isArray: true })
+  @Prop({ items: DeathPlace })
+  deathPlace: DeathPlace[];
+
+  @ApiPropertyOptional({ type: () => Profession, isArray: true })
+  @Prop({ items: Profession })
+  profession: Profession[];
+
+  constructor(person: Partial<MeiliPersonEntityV1_4>) {
+    super();
+    Object.assign(this, person);
+  }
+
+  public fromMongoDocument(person: Person): this {
+    this.id = person.id;
+    this.name = person.name || '';
+    this.enName = person.enName || '';
+    this.photo = person.photo || '';
+    this.sex = person.sex || '';
+    this.growth = person.growth || 0;
+    this.birthday = person.birthday || '';
+    this.death = person.death || '';
+    this.age = person.age || 0;
+
+    this.birthPlace = person.birthPlace || [];
+
+    this.deathPlace = person.deathPlace || [];
+
+    this.profession = person.profession || [];
+
+    return this;
+  }
+}
