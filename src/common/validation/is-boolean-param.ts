22be74416d3787a12a7d128466a5933b7e04363b
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-10-31 21:24:04 +0100

    chore: add is boolean validation

---
+++ src/common/validation/is-boolean-param.ts
@@ -0,0 +1,19 @@
+import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+
+@ValidatorConstraint({ name: 'IsBooleanParam', async: false })
+export class IsBooleanParam implements ValidatorConstraintInterface {
+  boolMap = {
+    true: true,
+    false: false,
+  };
+  validate(value: any, args: ValidationArguments) {
+    if (Array.isArray(value)) return value.every((item) => this.validate(item, args));
+
+    if (typeof value === 'boolean') return value;
+    if (typeof value !== 'string') return false;
+    return !!this.boolMap[value?.toLowerCase()];
+  }
+  defaultMessage(args: ValidationArguments) {
+    return `Значение поля ${args.property} должно быть булевым!`;
+  }
+}
