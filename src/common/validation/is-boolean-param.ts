701fdc80c9ad728e38b9d08b960bc73f395c4d5e
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-11-13 12:41:17 +0100

    fix: not correct false validate

--- src/common/validation/is-boolean-param.ts
+++ src/common/validation/is-boolean-param.ts
@@ -4,13 +4,14 @@ import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface 
 export class IsBooleanParam implements ValidatorConstraintInterface {
   boolMap = {
     true: true,
-    false: false,
+    false: true,
   };
   validate(value: any, args: ValidationArguments) {
     if (Array.isArray(value)) return value.every((item) => this.validate(item, args));
 
     if (typeof value === 'boolean') return value;
     if (typeof value !== 'string') return false;
+
     return !!this.boolMap[value?.toLowerCase()];
   }
   defaultMessage(args: ValidationArguments) {
