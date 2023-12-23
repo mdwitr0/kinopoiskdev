84dd0b712b311e64f63062576c2c07e2b36427c1
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-12-23 23:56:43 +0100

    chore: set not null props in swagger

--- src/person/dto/v1.4/person-find-one-params.dto.ts
+++ src/person/dto/v1.4/person-find-one-params.dto.ts
@@ -1,11 +1,11 @@
 import { IsOptional, Validate } from 'class-validator';
-import { ApiNullableProperty } from '../../../common/decorators/api-nullable-property.decorator';
 import { IsNumberParam } from '../../../common/validation/is-number-param';
 import { IsValueInRange } from '../../../common/validation/is-value-in-range';
 import { ToArray } from '../../../common/decorators/transform/to-array.decorator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PersonFindOneParamsDtoV1_4 {
-  @ApiNullableProperty({ isArray: true, description: 'ID из кинопоиска' })
+  @ApiProperty({ required: true, description: 'ID из кинопоиска' })
   @IsOptional()
   @ToArray()
   @Validate(IsValueInRange, [1, 30000000])
